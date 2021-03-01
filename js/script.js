5e18508cf17f01a9385ffd2358a9d36396e6ce64
PashaBasha <pachabaha@ukr.net>
2021-03-01 16:42:41 +0200

    update timer

--- js/script.js
+++ js/script.js
@@ -42,18 +42,46 @@ window.addEventListener('DOMContentLoaded', function () {
     })
 
     //timer
-   
+    let deadline = '2021-03-02'
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date());
+        let seconds = Math.floor((t / 1000 % 60))
+        let minutes = Math.floor((t / 1000 / 60) % 60)
+        let hourse = Math.floor((t / 1000 * 60 % 60))
+
+
+        return {
+            'total': t,
+            'hours': hourse,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id);
+        let hours = timer.querySelector('.hours');
+        let minutes = timer.querySelector('.minutes');
+        let seconds = timer.querySelector('.seconds');
+        let timeInterval = setInterval(updateClock, 1000);
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours + 'ч';
+            minutes.textContent = t.minutes + 'м';
+            seconds.textContent = t.seconds + 'с';
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+    }
+    setClock('timer', deadline);
+
+
 });
 
 
-let deadline = '2021-03-02'
-function getTimeRemaining(endtime) {
-    let t = Date.parse(endtime) - Date.parse(new Date());
-    let seconds = Math.floor((t / 1000 % 60))
-    console.log(seconds)
-}
 
-getTimeRemaining(deadline)
+
 
 
 
